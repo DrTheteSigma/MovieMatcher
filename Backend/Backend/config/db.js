21e87430bf848c001e7284b4e3169db2129e51f1
DrTheteSigma <halsailani@usf.edu>
2023-04-03 21:55:25 -0400

    Trying to host in aws

--- Backend/Backend/config/db.js
+++ Backend/Backend/config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const url = process.env.MONGO_URL
+const url = "mongodb+srv://hashem:hashem@matchercluster.skka9j9.mongodb.net/?retryWrites=true&w=majority"
 
 const connectDB  = async () =>{
     try{
